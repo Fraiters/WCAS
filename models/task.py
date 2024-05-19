91c2ad3ad262eb34a95161d8a8891db1c61e655d
Fraiters <dutovmv@mail.ru>
2024-05-19 14:48:49 +0300

    WCAS-21 Добавить возможность закрытия задачи
    1) Добавил хендлер /Задачи/Закрыть_задачу.
    2) Добавил изменение статуса задачи с `Doing` на `Done`.
    3) Добавил в класс `Task` поля `closing_date`- дата закрытия, `previous_task`, `next_task`.
    4) Добавил связанные задачи (предыдущую и следующую) в таблице `tasks` в БД и хендлер для их добавления.
    5) Добавил время закрытия в таблице `tasks` в БД
    6) Установил при закрытии задачи время закрытия.
    7) Заменил `priority` на `complexity`.

--- models/task.py
+++ models/task.py
@@ -1,5 +1,6 @@
 from typing import Dict, Union, Tuple
 from aiogram.types import Message
+from utils.utils import get_current_date
 
 
 class Task:
@@ -11,8 +12,11 @@ class Task:
         self.title = ...  # type: str
         self.description = ...  # type: str
         self.status = ...  # type: str
-        self.priority = ...  # type: str
+        self.complexity = ...  # type: str
         self.deadline = ...  # type: str
+        self.closing_date = ...  # type: str
+        self.previous_task = ...  # type: str
+        self.next_task = ...  # type: str
         self.executor_type = ...  # type: str
         self.executor_id = ...  # type: str
 
@@ -21,24 +25,28 @@ class Task:
         self.title = data.get("title")
         self.description = data.get("description")
         self.status = data.get("status")
-        self.priority = data.get("priority")
+        self.complexity = data.get("complexity")
         self.deadline = data.get("deadline")
-        self.executor_type = data.get("executor_type")
+        self.closing_date = data.get("closing_date")
+        self.previous_task = data.get("previous_task")
+        self.next_task = data.get("next_task")
 
-        if data.get("executor_id") is not None:
-            self.executor_id = data.get("executor_id")
-        else:
-            self.executor_id = None
+        self.executor_type = data.get("executor_type")
+        self.executor_id = data.get("executor_id")
 
     def from_tuple(self, data: Tuple):
         """Перевод из кортежа в данные 'задачи' """
-        for uuid, title, description, status, priority, deadline, executor_type, executor_id in [data]:
+        for uuid, title, description, status, complexity, deadline, closing_date, previous_task, next_task,\
+                executor_type, executor_id in [data]:
             self.uuid = uuid
             self.title = title
             self.description = description
             self.status = status
-            self.priority = priority
+            self.complexity = complexity
             self.deadline = deadline
+            self.closing_date = closing_date
+            self.previous_task = previous_task
+            self.next_task = next_task
             self.executor_type = executor_type
             self.executor_id = executor_id
 
@@ -48,8 +56,11 @@ class Task:
             "title": self.title,
             "description": self.description,
             "status": self.status,
-            "priority": self.priority,
+            "complexity": self.complexity,
             "deadline": self.deadline,
+            "closing_date": self.closing_date,
+            "previous_task": self.previous_task,
+            "next_task": self.next_task,
             "executor_type": self.executor_type,
             "executor_id": self.executor_id
         }
@@ -58,19 +69,34 @@ class Task:
     async def set_uuid(self, uuid: int):
         self.uuid = uuid
 
+    async def set_closing_date(self):
+        self.closing_date = get_current_date()
+
     async def print_info(self, message: Message):
         """Вывод всех данных о задачи в сообщении"""
 
         if self.executor_id is None:
             self.executor_id = "Не назначен"
 
+        if self.previous_task is None:
+            self.previous_task = "Нет"
+
+        if self.next_task is None:
+            self.next_task = "Нет"
+
+        if self.closing_date is None:
+            self.closing_date = "Не выполнена"
+
         await message.reply(f"Название задачи: {self.title}\n\n"
                             f"Описание: \n{self.description}\n"
                             f"Готовность: {self.status}\n"
-                            f"Приоритет: {self.priority}\n"
+                            f"Сложность: {self.complexity}\n"
                             f"Срок выполнения: {self.deadline}\n"
                             f"Тип исполнителя: {self.executor_type}\n"
-                            f"Исполнитель: @{self.executor_id}\n")
+                            f"Исполнитель: @{self.executor_id}\n\n"
+                            f"Предыдущая связанная задача (id): {self.previous_task}\n"
+                            f"Следующая связанная задача (id): {self.next_task}\n"
+                            f"Дата закрытия задачи: {self.closing_date}\n")
 
 
 # Статус задачи
--- utils/utils.py
+++ utils/utils.py
@@ -1,11 +1,17 @@
-import datetime
+from datetime import datetime
 
 
 def is_datetime(date: str) -> bool:
     """ Проверка формата даты """
     try:
-        datetime.datetime.strptime(date, '%d.%m.%Y')
+        datetime.strptime(date, '%d.%m.%Y')
         return True
 
     except Exception:
         return False
+
+
+def get_current_date():
+    """ Получение текущей даты в европейском формате """
+    current_date = datetime.now().strftime('%d.%m.%Y')
+    return current_date
